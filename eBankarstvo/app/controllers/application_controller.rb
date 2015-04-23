c2720d8d4687c13b96eebd3720b570a189253f55
Irma <irmabalic1@gmail.com>
2015-04-23 15:57:10 +0200

    .

--- eBankarstvo/app/controllers/application_controller.rb
+++ eBankarstvo/app/controllers/application_controller.rb
@@ -6,10 +6,9 @@ class ApplicationController < ActionController::Base
   def find_user
     if session[:user_id]
       puts "Session id: #{session[:user_id]}"
-      u = User.find(session[:user_id])
-      puts "User id: #{u.id}"
-      
-      @user = User.find_by(id: session[:user_id])
+      @user = User.find(session[:user_id])
+      #puts "User id: #{u.id}"  
+     # @user = User.find_by(id: session[:user_id])
     end
   end
 end
